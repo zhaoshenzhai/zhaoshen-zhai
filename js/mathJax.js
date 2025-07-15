03d4def10f1dfb8a65ae07476f1a4d3040f64936
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2025-07-15 11:45:41 -0400

    mathjax

--- js/mathJax.js
+++ js/mathJax.js
@@ -10,14 +10,7 @@ window.MathJax = {
 };
 
 (function() {
-    fetch('./macros.sty')
-        .then(response => response.text())
-        .then((data) => {
-            var preamble = document.getElementById('preamble');
-            if (preamble) { preamble.innerHTML = '$' + data + '$'; }
-
-            var script = document.createElement('script');
-            script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-            document.head.appendChild(script);
-        });
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    document.head.appendChild(script);
 })();
